2da000dd36a998ebea04a9fee51c3532285c54eb
zsctest <1607192614@qq.com>
2023-04-07 10:45:20 +0800

    完成第1-4关

--- rce/app.py
+++ rce/app.py
@@ -1,5 +1,7 @@
 from flask import Flask,request
 from flask import render_template
+from utils import *
+
 import subprocess
 
 app = Flask(__name__)
@@ -10,22 +12,59 @@ def index():
 
 @app.route("/first",methods=['GET'])
 def first():
-    return render_template("first.html")
+    return render_template("/views/first.html")
 
 @app.route('/first',methods=['POST'])
 def ping():
     ip_address = request.form['inputip']
     command = 'ping -c 3 %s'%ip_address
     status,output = subprocess.getstatusoutput(command)
-    return render_template('first.html', output=output)
+    return render_template('/views/first.html', output=output)
 
-@app.route("/second")
+@app.route("/second",methods=['GET'])
 def second():
-    return render_template("second.html")
+    return render_template("/views/second.html")
 
-@app.route("/third")
+@app.route("/second",methods=['POST'])
+def second2():
+    status,output = ping2(request,filter_spaces)
+    return render_template("/views/second.html",output=output,detail="空格")
+
+@app.route("/third",methods=['GET'])
 def third():
-    return render_template("third.html")
+    return render_template("/views/third.html")
+
+
+@app.route("/third",methods=['POST'])
+def third2():
+    status,output = ping2(request,double_quotation)
+    return render_template("/views/third.html",output=output,detail="双引号")
+
+
+@app.route("/fourth",methods=['GET'])
+def fourth():
+    return render_template('/views/fourth.html')
+
+@app.route("/fourth",methods=['POST'])
+def fourth2():
+    status,output = ping2(request,single_quotation)
+    return render_template("/views/fourth.html",output=output,detail="单引号")
+
+
+@app.route("/fifth",methods=['GET'])
+def fifth():
+    return render_template('/views/fifth.html')
+
+@app.route("/fifth",methods=['POST'])
+def fifth2():
+    status,output = curl(request,filter_quotation)
+    return render_template("/views/fifth.html",output=output,detail="过滤引号")
+
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
 
 
 
